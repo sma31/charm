79c4debf5ded676bd3a2c117e88d77d8c0e53a81
Nitin Bhat <nbhat4@illinois.edu>
2019-04-25 09:36:44 -0500

    ZC API: Rename registration mode variable from mode to regMode
    Change-Id: I9984ae6a8d48fe69fb441a6d785313118a26f594

--- src/ck-core/init.C
+++ src/ck-core/init.C
@@ -979,7 +979,7 @@ static void _roRdmaDoneHandler(envelope *env) {
           CmiDeregisterMem(buffAckInfo->ptr,
                            buffAckInfo->layerInfo +CmiGetRdmaCommonInfoSize(),
                            buffAckInfo->pe,
-                           buffAckInfo->mode);
+                           buffAckInfo->regMode);
         }
 
         if(roBcastAckInfo->isRoot != 1) {
