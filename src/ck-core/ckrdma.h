6b25ad55193a0ba71bf596c614b9dec93bb5ef95
Nitin Bhat <nbhat4@illinois.edu>
2019-06-03 14:20:12 -0500

    ZC RO Bcast: QD Support

--- src/ck-core/ckrdma.h
+++ src/ck-core/ckrdma.h
@@ -580,6 +580,8 @@ void CkRdmaEMDeregAndAckHandler(void *ack);
 
 inline bool isDeregReady(CkNcpyBuffer &buffInfo);
 
+inline void zcQdIncrement();
+
 #endif /* End of CMK_ONESIDED_IMPL */
 
 #endif
